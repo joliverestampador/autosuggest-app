944ecee825a3b46213a501cc9c10986ae06baa6d
Joliver Estampador <joliver.estampador@tooltwist.com>
2021-09-17 15:06:27 +0800

    fixed failing tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import { shallow } from 'enzyme';
+import logo from './images/logo.png';
+import AutoSuggestContainer from './components/AutoSuggest';
+import Watchlist from './components/Watchlist';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+    let wrapper;
+    beforeAll(() => {
+        wrapper = shallow(<App />);
+    });
+
+    it('renders logo', () => {
+        expect(wrapper.find('Image')).toHaveProp({
+            src: logo,
+            height: '150',
+            width: '150',
+            alt: 'logo',
+        });
+    });
+
+    it('renders AutoSuggestContainer', () => {
+        expect(wrapper.find(AutoSuggestContainer)).toExist();
+    });
+
+    it('renders Watchlist', () => {
+        expect(wrapper.find(Watchlist)).toExist();
+    });
 });
